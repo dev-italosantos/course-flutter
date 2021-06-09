d931ea42d6ad960ff0e56abf81092ee02d85a192
Ítalo Santos <italotayllor@gmail.com>
2021-06-09 20:10:07 -0300

    course-flutter-ordering-the-items

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,23 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((x, y) {
+        if (x["ok"] && y["ok"])
+          return 1;
+        else if (!x["ok"] && y["ok"])
+          return -1;
+        else
+          return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,10 +101,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: _toDoList.length,
-                itemBuilder: buildItem),
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
+            ),
           ),
         ],
       ),
