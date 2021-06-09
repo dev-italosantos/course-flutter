bd2f474c36c90af1318aabcfd15a5087df9dabf2
Ítalo Santos <italotayllor@gmail.com>
2021-06-09 16:11:43 -0300

    course-flutter-saving-the-item-permanently

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,17 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) => {
+          setState(() {
+            _toDoList = json.decode(data);
+          })
+        });
+  }
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -28,6 +39,8 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+
+      _saveData();
     });
   }
 
@@ -77,6 +90,7 @@ class _HomeState extends State<Home> {
                   onChanged: (value) {
                     setState(() {
                       _toDoList[index]["ok"] = value;
+                      _saveData();
                     });
                   },
                   title: Text(_toDoList[index]["title"]),
