f66242f8ee1431fdcd983391818f18b167d2e554
Ítalo Santos <italotayllor@gmail.com>
2021-07-16 23:53:12 -0300

    couser-udemy

---
+++ lib/ui/conversor_moedas/conversor_moedas.dart
@@ -0,0 +1,11 @@
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// const request = "https://api.hgbrasil.com/finance?format=json&key=db0c52ea";
+
+// void conversor() async {
+//   http.Response response = await http.get(
+//     Uri.parse(request),
+//   );
+//   print(json.decode(response.body)["results"]["currencies"]["USD"]);
+// }
